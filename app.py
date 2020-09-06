314318ccaf9e13b1c3d50cf5a8b42aa936c37da5
Jakub Dembowski <54886151+JakubDembowski@users.noreply.github.com>
2020-09-06 14:45:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
@@ -13,5 +14,15 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """ .format(time=the_time)
 
+@app.route('/sida2')
+def page2():
+    return """
+    <h1>Þetta er önnur síða</h1>
+    <p><a href="/">Forsíða</a></p>
+    <p>It is currently {time}.</p> 
+    <p><a href="/sida2">Síða 2 </a> | <a href="/sida3" title="Síða 3">Síða 3 </a></p>
+    <img src="http://loremflickr.com/600/400" />"""
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    #app.run(debug=True, use_reloader=True)
+    app.run()
